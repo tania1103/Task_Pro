7d4062016df638a62b2aa21839886de03cba544c
Tatiana Culeac <culeactatiana1103@gmail.com>
2025-06-21 20:25:36 +0300

    Prepared to work

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import { Provider } from 'react-redux';
 import 'modern-normalize';
 import GlobalStyles from 'assets/styles/GlobalStyles';
 import './index.css';
-import App from 'components/App';
+import App from './components/App/App';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-import '../src/assets/i18/i18';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
