b89d24b5025942d38f1322ab812f7ccfc4eca74c
Alexandra Stavila <109295061+AlexandraMdv@users.noreply.github.com>
2025-06-26 12:26:52 +0300

    App.js merged correct

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,56 +1,94 @@
+import { Suspense, lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAuth } from 'hooks';
+import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import GlobalStyles from 'assets/styles/GlobalStyles';
-import MainPage from 'pages/MainPage';
+import { refreshUser } from '../../redux/auth/authOperations';
+import { PrivateRoute } from '../../routes/PrivateRoute';
+import { PublicRoute } from '../../routes/PublicRoute';
+import SharedLayout from 'layouts/SharedLayout';
+import Loader from 'components/Loader/Loader';
+
+const WelcomePage = lazy(() => import('pages/WelcomePage'));
+const AuthPage = lazy(() => import('pages/AuthPage'));
+const HomePage = lazy(() => import('pages/HomePage'));
+const ScreensPage = lazy(() => import('pages/ScreensPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
+const StatsPage = lazy(() => import('pages/StatsPage'));
+const SchedulePage = lazy(() => import('pages/SchedulePage'));
 
 const App = () => {
   const dispatch = useDispatch();
-  const { currentTheme } = useThemeManager();
-  const theme = getTheme(currentTheme);
+  const { isRefreshing } = useAuth();
 
-  // Check if user is logged in on app load
   useEffect(() => {
-    const token = localStorage.getItem('taskpro_token');
-    if (token) {
-      dispatch(refreshToken(token))
-        .unwrap()
-        .then(() => {
-          // Fetch boards after successful authentication
-          dispatch(fetchBoards());
-        })
-        .catch(() => {
-          // Token is invalid, do nothing
-        });
-    }
+    dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
+  return isRefreshing ? (
+    <Loader strokeColor="#fff" />
+  ) : (
+    <>
+      <Toaster position="top-center" />
+
+      <Suspense fallback={<Loader />}>
         <Routes>
-          {/* Public Routes */}
-          <Route element={<PublicRoute />}>
-            <Route element={<AuthLayout />}>
-              <Route path="/auth/login" element={<LoginPage />} />
-              <Route path="/auth/register" element={<RegisterPage />} />
-            </Route>
+          <Route
+            path="/"
+            element={
+              <PublicRoute component={<WelcomePage />} redirectTo="/home" />
+            }
+          />
+          <Route
+            path="/auth/:id"
+            element={
+              <PublicRoute component={<AuthPage />} redirectTo="/home" />
+            }
+          />
+          <Route path="/home" element={<SharedLayout />}>
+            <Route
+              index
+              element={
+                <PrivateRoute
+                  component={<HomePage />}
+                  redirectTo={'/auth/login'}
+                />
+              }
+            />
+            <Route
+              path="board/:boardId"
+              element={
+                <PrivateRoute
+                  component={<ScreensPage />}
+                  redirectTo={'/auth/login'}
+                />
+              }
+            />
+            <Route
+              path="stats"
+              element={
+                <PrivateRoute
+                  component={<StatsPage />}
+                  redirectTo={'/auth/login'}
+                />
+              }
+            />
+            <Route
+              path="schedule"
+              element={
+                <PrivateRoute
+                  component={<SchedulePage />}
+                  redirectTo={'/auth/login'}
+                />
+              }
+            />
           </Route>
 
-          {/* Private Routes */}
-          <Route element={<PrivateRoute />}>
-            <Route element={<AppLayout />}>
-              <Route path="/" element={<DashboardPage />} />
-              <Route path="/boards/:boardId" element={<BoardPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/help" element={<HelpPage />} />
-            </Route>
-          </Route>
-
-          {/* Redirect to login for unknown routes */}
-          <Route path="*" element={<Navigate to="/auth/login" replace />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </Router>
-    </ThemeProvider>
+      </Suspense>
+    </>
   );
 };
+
 export default App;
